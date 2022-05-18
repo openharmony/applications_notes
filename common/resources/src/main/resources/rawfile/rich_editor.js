cf2708d538b0af4082b7f17556c11adb33a9b383
guozejun <guozejun@huawei.com>
2022-05-18 15:22:41 +0800

    adapt project structure for API9
    Signed-off-by: guozejun <guozejun@huawei.com>

---
+++ common/resources/src/main/resources/rawfile/rich_editor.js
@@ -0,0 +1,348 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var RICH_EDITOR = {};
+
+RICH_EDITOR.editor = document.getElementById('editorjs');
+
+RICH_EDITOR.setHtml = function(contents) {
+    RICH_EDITOR.editor.innerHTML = contents.replace(/%20/g, '+');
+}
+
+RICH_EDITOR.getHtml = function() {
+    return RICH_EDITOR.editor.innerHTML;
+}
+
+RICH_EDITOR.undo = function() {
+    document.execCommand('undo', false, null);
+}
+
+RICH_EDITOR.redo = function() {
+    document.execCommand('redo', false, null);
+}
+
+RICH_EDITOR.setBold = function() {
+    document.execCommand('bold');
+}
+
+RICH_EDITOR.setItalic = function() {
+    document.execCommand('italic', false, null);
+}
+
+RICH_EDITOR.setSubscript = function() {
+    document.execCommand('subscript', false, null);
+}
+
+RICH_EDITOR.setSuperscript = function() {
+    document.execCommand('superscript', false, null);
+}
+
+RICH_EDITOR.setStrikeThrough = function() {
+    document.execCommand('strikeThrough', false, null);
+}
+
+RICH_EDITOR.setUnderline = function() {
+    document.execCommand('underline', false, null);
+}
+
+RICH_EDITOR.getListStyle = function () {
+    var selection, type;
+    if (window.getSelection) {
+        selection = getSelection();
+    }
+    if (selection) {
+        var range = selection.getRangeAt ? selection.getRangeAt(0) : selection.createRange();
+        try {
+            var child = range.commonAncestorContainer.parentNode;
+            for (var i = 0; i < 10; i++) {
+                if (child.nodeName == "OL") {
+                    console.info('insertOrderedList')
+                    document.execCommand('insertOrderedList', false, null);
+                    return child.style["list-style"]
+                }
+                if (child.nodeName == "UL") {
+                    console.info('insertUnorderedList')
+                    document.execCommand('insertUnorderedList', false, null);
+                    return child.style["list-style"]
+                }
+                if (child.parentNode) {
+                    child = child.parentNode
+                }
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+}
+
+RICH_EDITOR.setNumbers = function () {
+    let listStyle = RICH_EDITOR.getListStyle()
+    if(listStyle == "decimal") {
+        return
+    }
+    document.execCommand('insertOrderedList', false, null);
+    var selection, type;
+    if (window.getSelection) {
+        selection = getSelection();
+    }
+    if (selection) {
+        var range = selection.getRangeAt ? selection.getRangeAt(0) : selection.createRange();
+        try {
+            var child = range.commonAncestorContainer.parentNode;
+            for (var i = 0; i < 10; i++) {
+                if (child.nodeName == "OL") {
+                    child.style["list-style"] = "decimal";
+                    break;
+                }
+                if (child.parentNode) {
+                    child = child.parentNode
+                }
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
+
+RICH_EDITOR.setABC = function () {
+    let listStyle = RICH_EDITOR.getListStyle()
+    if(listStyle == "lower-alpha") {
+        return
+    }
+    document.execCommand('insertOrderedList', false, null);
+    var selection, type;
+    if (window.getSelection) {
+        selection = getSelection();
+    }
+    if (selection) {
+        var range = selection.getRangeAt ? selection.getRangeAt(0) : selection.createRange();
+        try {
+            var child = range.commonAncestorContainer.parentNode;
+            for (var i = 0; i < 10; i++) {
+                if (child.nodeName == "OL") {
+                    child.style["list-style"] = "lower-alpha";
+                    break;
+                }
+                if (child.parentNode) {
+                    child = child.parentNode
+                }
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
+
+RICH_EDITOR.setBullets = function () {
+    let listStyle = RICH_EDITOR.getListStyle()
+    if(listStyle == "disc") {
+        return
+    }
+    document.execCommand('insertUnorderedList', false, null);
+    var selection, type;
+    if (window.getSelection) {
+        selection = getSelection();
+    }
+    if (selection) {
+        var range = selection.getRangeAt ? selection.getRangeAt(0) : selection.createRange();
+        try {
+            var child = range.commonAncestorContainer.parentNode;
+            for (var i = 0; i < 10; i++) {
+                if (child.nodeName == "UL") {
+                    child.style["list-style"] = "disc";
+                    break;
+                }
+                if (child.parentNode) {
+                    child = child.parentNode
+                }
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
+
+RICH_EDITOR.setSquare = function () {
+    let listStyle = RICH_EDITOR.getListStyle()
+    if(listStyle == "square") {
+        return
+    }
+    document.execCommand('insertUnorderedList', false, null);
+    var selection, type;
+    if (window.getSelection) {
+        selection = getSelection();
+    }
+    if (selection) {
+        var range = selection.getRangeAt ? selection.getRangeAt(0) : selection.createRange();
+        try {
+            var child = range.commonAncestorContainer.parentNode;
+            for (var i = 0; i < 10; i++) {
+                if (child.nodeName == "UL") {
+                    child.style["list-style"] = "square";
+                    break;
+                }
+                if (child.parentNode) {
+                    child = child.parentNode
+                }
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
+
+RICH_EDITOR.setTextColor = function (color) {
+    document.execCommand('foreColor', false, color);
+}
+
+RICH_EDITOR.setFontSize = function(fontSize){
+    document.execCommand("fontSize", false, fontSize);
+}
+
+RICH_EDITOR.execFontSize = function (size, unit) {
+    document.execCommand("fontSize", false, "7");
+    var fontElements = window.getSelection().anchorNode.parentNode
+    fontElements.removeAttribute("size");
+    fontElements.style.fontSize = size + 'px'
+};
+
+var pad = 24
+RICH_EDITOR.setIndent = function() {
+    var parents = document.getElementById('editorjs')
+    parents.removeAttribute('padding-left');
+    pad = pad + 24
+    parents.style.paddingLeft = pad + 'px'
+}
+
+RICH_EDITOR.setOutdent = function() {
+    var parents = document.getElementById('editorjs')
+    parents.removeAttribute('padding-left');
+    if (pad == 24) {
+        parents.style.paddingLeft = 24 + 'px'
+    } else {
+        pad = pad - 24
+        parents.style.paddingLeft = pad + 'px'
+    }
+}
+
+RICH_EDITOR.setJustifyLeft = function() {
+    document.execCommand('justifyLeft', false, null);
+}
+
+RICH_EDITOR.setJustifyCenter = function() {
+    document.execCommand('justifyCenter', false, null);
+}
+
+RICH_EDITOR.setJustifyRight = function() {
+    document.execCommand('justifyRight', false, null);
+}
+
+RICH_EDITOR.insertImage = function(url) {
+    var html = '<br></br><img src="' + url + '" alt="picvision" style="margin:0px auto;width:90%;display:table-cell;vertical-align:middle;border-radius:10px;max-width:90%" /><br></br>';
+    RICH_EDITOR.insertHTML(html);
+    RICH_EDITOR.editor.scrollIntoView(false)
+}
+
+RICH_EDITOR.insertHTML = function(html) {
+    document.execCommand('insertHTML', false, html);
+}
+
+RICH_EDITOR.setDone = function() {
+    var html = '<input type="checkbox" checked="checked"/> &nbsp;';
+    document.execCommand('insertHTML', false, html);
+}
+
+RICH_EDITOR.addTodo=function(e){
+    KEY_ENTER=13;
+    if(e.which == KEY_ENTER){
+        var node=RICH_EDITOR.getSelectedAnchorNode();
+        if(node&&node.nodeName=="#text"){
+            node=node.parentElement;
+        }
+        if(node&&node.nodeName=="SPAN"&&node.previousElementSibling&&node.previousElementSibling.className=='note-checkbox'){
+            RICH_EDITOR.setTodo();
+            e.preventDefault();
+        }
+    }
+}
+
+RICH_EDITOR.setTodo = function () {
+    var parent = document.getElementById('editorjs')
+    var isContentEmpty = parent.innerHTML.trim().length == 0 || parent.innerHTML == '<br>';
+    var html = (isContentEmpty ? '' : '<br/>') + '<input name="checkbox" type="checkbox" onclick="onCheckChange(this)" class="note-checkbox"><span class="note-checkbox-txt">&nbsp;</span>';
+    document.execCommand('insertHTML', false, html);
+}
+
+function onCheckChange(checkbox) {
+    if (checkbox.checked == true) {
+        checkbox.setAttribute("checked", "checked");
+    } else {
+        checkbox.removeAttribute("checked");
+    }
+}
+
+RICH_EDITOR.restorerange = function(){
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    var range = document.createRange();
+    range.setStart(RICH_EDITOR.currentSelection.startContainer, RICH_EDITOR.currentSelection.startOffset);
+    range.setEnd(RICH_EDITOR.currentSelection.endContainer, RICH_EDITOR.currentSelection.endOffset);
+    selection.addRange(range);
+}
+
+//获取光标开始位置归属节点
+RICH_EDITOR.getSelectedAnchorNode=function(){
+    var node,selection;
+    if (window.getSelection) {
+        selection = getSelection();
+        node = selection.anchorNode;
+    }
+    if (!node && document.selection) {
+        selection = document.selection
+        var range = selection.getRangeAt ? selection.getRangeAt(0) : selection.createRange();
+        node = range.commonAncestorContainer ? range.commonAncestorContainer :
+                range.parentElement ? range.parentElement() : range.item(0);
+    }
+    return node;
+}
+
+function get_html_content() {
+    console.log('get_html_content');
+    var htmlString =  RICH_EDITOR.getHtml()
+    var str = callBackToApp.callbackhtml(htmlString)
+    console.log('get_html_content end');
+}
+
+function save_html_content() {
+    console.log('save_html_content');
+    var htmlString =  RICH_EDITOR.getHtml()
+    var str = callBackToApp.callbackhtmlSave(htmlString.replace(/\+/g, '%20'))
+    console.log('save_html_content end');
+}
+
+function scheduled_save_content() {
+    console.info('scheduled_save_content')
+    var htmlString = RICH_EDITOR.getHtml()
+    var str = callBackToApp.callbackScheduledSave(htmlString)
+    console.info('scheduled_save_content end')
+}
+
+RICH_EDITOR.getFontSizes = function(){
+    document.execCommand("fontSize", false, null);
+    var fontElements = window.getSelection().anchorNode.parentNode
+    var getSize = fontElements.style.fontSize
+    var str = callBackToApp.callbackGetSize(getSize)
+}
--- product/tablet/src/main/ets/MainAbility/MainAbility.ts
+++ product/tablet/src/main/ets/MainAbility/MainAbility.ts
@@ -14,32 +14,32 @@
  */
 
 import Ability from '@ohos.application.Ability'
-import {WebViewUtil} from './WebViewUtil.ets'
+import AbilityConstant from '@ohos.application.AbilityConstant'
 import fileio from '@ohos.fileio'
+import inputMethod from '@ohos.inputmethod';
 
 export default class MainAbility extends Ability {
-    private Tag = "Tablet_Note_MainAbility"
+    private Tag = "MainAbility_Tablet"
 
     onCreate(want, launchParam) {
         console.info(this.Tag + " onCreate, launchReason is " + launchParam.launchReason)
-        if (launchParam.launchReason == 3) {
+        if (launchParam.launchReason == AbilityConstant.LaunchReason.CONTINUATION) {
+            // 设置迁移标记
+            AppStorage.SetOrCreate<boolean>('IsContinue', true)
             // 获取对端的迁移数据
             let continueNote: string = want.parameters["ContinueNote"]
             let continueSection: number = want.parameters["ContinueSection"]
             console.info(this.Tag + " continueSection : " + continueSection)
             AppStorage.SetOrCreate<string>('ContinueNote', continueNote)
             AppStorage.SetOrCreate<number>('ContinueSection', continueSection)
-            // 设置迁移标记
-            AppStorage.SetOrCreate<boolean>('IsContinue', true)
-
             // 来自手机的迁移
-            let isChoose: boolean = want.parameters["ContinueChoose"]
-            console.info(this.Tag + " from phone, isChoose : " + isChoose)
-            AppStorage.SetOrCreate<boolean>('IsChoose', isChoose)
+            let continueChoose: boolean = want.parameters["ContinueChoose"]
+            if (continueChoose) {
+                console.info(this.Tag + " continue from phone")
+                AppStorage.SetOrCreate<boolean>('ContinueFromPhone', true)
+            }
             this.context.restoreWindowStage(null)
         }
-        AppStorage.SetOrCreate<number>('openPhoto', 0)
-        AppStorage.SetOrCreate<number>('openPerm', 0)
         globalThis.noteContext = this.context
     }
 
@@ -62,14 +62,76 @@ export default class MainAbility extends Ability {
 
     onBackground() {
         console.info(this.Tag + " onBackground")
-        let controllerShow = WebViewUtil.getWebController()
-        if (controllerShow == undefined || controllerShow == null) {
-            console.info("MainAbility onBackground, controllerShow is error")
+        // 退出键盘
+        inputMethod.getInputMethodController().stopInput();
+    }
+
+    onContinue(wantParam: { [key: string]: any }) {
+        console.info(this.Tag + " onContinue")
+        // 获取本端的迁移数据
+        let continueNote = AppStorage.Get<string>('ContinueNote')
+        if (continueNote == undefined || continueNote == null) {
+            console.info(this.Tag + " onContinue, continueNote is error, default [0]")
+            continueNote = JSON.stringify(AppStorage.Get('AllNoteArray')[0].toNoteObject())
         }
-        console.info(this.Tag + " controllerShow : " + controllerShow)
-        controllerShow.runJavaScript({
-            script: "get_html_content()"
+
+        let continueSection = AppStorage.Get<number>('ContinueSection')
+        if (continueSection == undefined || continueSection == null) {
+            console.info(this.Tag + " onContinue, continueSection is error, default 3")
+            continueSection = 3
+        }
+        console.info(this.Tag + " onContinue, continueSection : " + continueSection)
+
+        // 保存本端的迁移数据
+        wantParam["ContinueNote"] = continueNote
+        wantParam["ContinueSection"] = continueSection
+
+        // save img to DisFileDir
+        console.info(this.Tag + " onContinue, save img to DisFileDir")
+        let continueNoteObj = JSON.parse(continueNote)
+        let srcArray = this.getSrcFromHtml(continueNoteObj.content_text)
+        srcArray.forEach((src: string) => {
+            let lastIndex = src.lastIndexOf('/')
+            if (lastIndex != -1) {
+                let imgName = src.substring(lastIndex + 1)
+                this.writeToDisFileDir(imgName)
+            }
         })
-        console.info(this.Tag + " controllerShow end")
+        console.info(this.Tag + " onContinue end")
+        return AbilityConstant.OnContinueResult.AGREE
+    }
+
+    getSrcFromHtml(html: string): any{
+        let srcArray = []
+        if (html == undefined || html == null || html == "") {
+            return srcArray
+        }
+        let imgReg = /<img[^>]+>/g
+        let srcReg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i
+        let imgArray = html.match(imgReg)
+        if (imgArray != null) {
+            for (let i = 0; i < imgArray.length; i++) {
+                let src = imgArray[i].match(srcReg)
+                if (src != null && src.length > 1) {
+                    console.info(this.Tag + " getSrcFromHtml, src[1] : " + src[1])
+                    srcArray.push(src[1])
+                }
+            }
+        }
+        return srcArray
+    }
+
+    writeToDisFileDir(fileName: string) {
+        console.info(this.Tag + " writeToDisFileDir, fileName : " + fileName)
+        let filesDir = this.context.filesDir
+        let srcPath = filesDir + "/" + fileName
+        let distributedFilesDir = this.context.distributedFilesDir
+        let desPath = distributedFilesDir + "/" + fileName
+        try {
+            fileio.copyFileSync(srcPath, desPath)
+            console.info(this.Tag + " onContinue, writeToDisFileDir, copyFile successfully")
+        } catch (err) {
+            console.warn(this.Tag + " onContinue, writeToDisFileDir, copyFile failed : " + err)
+        }
     }
 }
